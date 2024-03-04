0bea51b407495a104f9ad613b3b1bf5477acc058
abhinav <abhinavguptabit@gmail.com>
2024-03-04 12:03:12 +0530

    Initial Commit

---
+++ toasterLibrary/src/main/java/com/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,12 @@
+package com.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+
+    fun toast(context: Context, message: String?){
+        Toast.makeText(context , message,Toast.LENGTH_SHORT).show()
+    }
+
+}
